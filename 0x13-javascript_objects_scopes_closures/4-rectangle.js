e18bb4edd459b62193997f40db264f27beb392ca
Jhofix <dadajoshua518@gmail.com>
2024-07-13 05:10:57 +0100

    tasks

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,36 +1,35 @@
 #!/usr/bin/node
 
 class Rectangle {
-    constructor (w, h) {
-      if (w > 0 && h > 0) {
-        this.width = w;
-        this.height = h;
-      }
-    }
-  
-    print () {
-      const row = this.height;
-      const column = this.width;
-      for (let x = 0; x < row; x++) {
-        let tmpStr = '';
-        for (let y = 0; y < column; y++) {
-          tmpStr += 'X';
-        }
-        console.log(tmpStr);
-      }
-    }
-  
-    rotate () {
-      const tmp = this.height;
-      this.height = this.width;
-      this.width = tmp;
-    }
-  
-    double () {
-      this.height *= 2;
-      this.width *= 2;
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
   }
-  
-  module.exports = Rectangle;
-  
+
+  print () {
+    const row = this.height;
+    const column = this.width;
+    for (let x = 0; x < row; x++) {
+      let tmpStr = '';
+      for (let y = 0; y < column; y++) {
+        tmpStr += 'X';
+      }
+      console.log(tmpStr);
+    }
+  }
+
+  rotate () {
+    const tmp = this.height;
+    this.height = this.width;
+    this.width = tmp;
+  }
+
+  double () {
+    this.height *= 2;
+    this.width *= 2;
+  }
+}
+
+module.exports = Rectangle;
