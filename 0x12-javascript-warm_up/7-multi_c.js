b28d58b227970539ba6162cb06c23b7292b411ae
Jhofix <dadajoshua518@gmail.com>
2024-08-10 08:14:36 +0100

    "task7"

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -5,9 +5,6 @@ const errtext = 'Missing number of occurrences';
 
 if (process.argv[2] > 0) {
   console.log(text);
-} else {
-    if (process.argv[2] <= 0) {
-      return;
-    }
-    console.log(errtext);
+} else if (isNaN(process.argv[2])) {
+  console.log(errtext);
 }
