dedf051ae7d87028b4b23d75043e46727963a671
Jhofix <dadajoshua518@gmail.com>
2024-08-15 13:04:28 +0100

    "task2"

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+'''  Prints only states contaning a search string'''
+
+import MySQLdb as mysql
+import sys
+
+
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    tag = sys.argv[4]
+
+    db = mysql.connect(host='localhost',
+                       user=username,
+                       passwd=password,
+                       db=database)
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name='" + tag + "'")
+    data = cursor.fetchall()
+    print("SELECT * FROM states WHERE name='" + tag + "'")
+    for elem in data:
+        print(elem)
+    cursor.close()
+    db.close()
