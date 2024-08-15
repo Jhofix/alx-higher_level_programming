ce332c8bdc19014718d0abfca9727cb681ad55ce
Jhofix <dadajoshua518@gmail.com>
2024-08-15 09:08:40 +0100

    "task1"

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
 """  lists all states with a name starting with N (upper N)
  from the database hbtn_0e_0_usa
-
-Attributes:
-    username: variable username
-    password: variable password
-    database: variable database
  """
 
 import MySQLdb as mysql
