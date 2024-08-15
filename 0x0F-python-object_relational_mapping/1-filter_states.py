f031a52b7970e70685fb743d25fd7bf50c8cd0e9
Jhofix <dadajoshua518@gmail.com>
2024-08-15 09:02:17 +0100

    "task1"

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -2,7 +2,7 @@
 """  lists all states with a name starting with N (upper N)
  from the database hbtn_0e_0_usa
 
- Attributes:
+Attributes:
     username: variable username
     password: variable password
     database: variable database
