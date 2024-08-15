7e1eadaf7b82260674870ef156e8c8d0ff42e04d
Jhofix <dadajoshua518@gmail.com>
2024-08-15 13:08:47 +0100

    "task2"

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -18,7 +18,7 @@ if __name__ == '__main__':
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states WHERE name='" + tag + "'")
     data = cursor.fetchall()
-    print("SELECT * FROM states WHERE name='" + tag + "'")
+    print("SELECT * FROM states WHERE name='%s'".format(tag))
     for elem in data:
         print(elem)
     cursor.close()
