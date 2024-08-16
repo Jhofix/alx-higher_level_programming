65de46338be6e182d19d818b880c1a5af6fc65b9
Jhofix <dadajoshua518@gmail.com>
2024-08-16 02:30:07 +0100

    "tasks"

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" prints the State object with the name passed as argument from the database
+"""Start link class to table in database
 """
 import sys
 from model_state import Base, State
@@ -13,8 +13,5 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    instance = session.query(State).filter(State.name == (sys.argv[4],))
-    try:
-        print(instance[0].id)
-    except IndexError:
-        print("Not found")
+    for instance in session.query(State).order_by(State.id):
+        print(instance.id, instance.name, sep=": ")
